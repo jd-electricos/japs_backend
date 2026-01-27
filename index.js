073decd237483c07f72c054a67a9ffc0ceb7866a
dispositivosjdelectrico2022 <dispositivosjdelectricos2022@gmail.com>
2026-01-27 08:28:18 -0500

    add atributes

--- index.js
+++ index.js
@@ -2,26 +2,44 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+
 const productRoutes = require('./routes/productRoutes.js');
 const emailRoutes = require('./routes/emailRoutes.js');
 const mapsRoutes = require('./routes/mapsRoutes.js');
 const postsRoutes = require('./routes/postsRoutes.js')
 const recaptchaRoutes = require('./routes/recaptchaRoutes.js')
+
 const sequelize = require('./config/sequelize.js');
+
 const app = express();
-// Middleware
-app.use(cors());
+
+// seguridad
+app.use(helmet());
+
+// app.use(cors());
+app.use(cors({
+  origin: ['https://jap.com.co'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+/* 📦 JSON */
 app.use(express.json());
+
 // Rutas
+// Middleware
 app.use('/api/products', productRoutes);
 app.use('/api/email', emailRoutes);
 app.use('/api/recaptcha', recaptchaRoutes);
 app.use('/api/maps', mapsRoutes);
 app.use('/api/posts', postsRoutes);
+
 // Sincronizar modelos con la base de datos
-sequelize.sync({ alter: true })
+sequelize.sync({ alter: false })
   .then(() => console.log('Tablas sincronizadas con éxito'))
   .catch(err => console.error('Error al sincronizar tablas:', err));
+
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
